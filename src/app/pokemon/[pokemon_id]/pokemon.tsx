aa29cc3e2cf7624b67476640158bee2f74ac6695
ardasaritas <arda.saritas@ug.bilkent.edu.tr>
2024-06-02 20:43:54 +0300

    trying the accordion

--- src/app/pokemon/[pokemon_id]/pokemon.tsx
+++ src/app/pokemon/[pokemon_id]/pokemon.tsx
@@ -27,3 +27,24 @@ export default function PokemonComponent(props : Props) {
        </Container>
    );
 }
+
+import Accordion from 'react-bootstrap/Accordion';
+
+function AllCollapseExample() {
+  return (
+    <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Strength
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Strength
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
