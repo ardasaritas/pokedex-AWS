f5e4af34ee14ff5c6a46e7c8709a7a184579b554
ardasaritas <arda.saritas@ug.bilkent.edu.tr>
2024-06-04 16:37:23 +0300

    draft for the final version

--- src/app/pokemon/[pokemon_id]/pokemon.tsx
+++ src/app/pokemon/[pokemon_id]/pokemon.tsx
@@ -1,15 +1,18 @@
 import Pokemon from "@/model/pokemon";
 import { Row, Col, Container, Image } from 'react-bootstrap';
-
+import Accordion from 'react-bootstrap/Accordion';
+import Stack from 'react-bootstrap/Stack';
+import Badge from 'react-bootstrap/Badge';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import './style.css'
 
 type Props ={
    pokemon: Pokemon;
 }
 
 
-export default function PokemonComponent(props : Props) {
+function PokemonComponent(props : Props) {
    const {pokemon} = props;
-
 
    return (
        <Container>
@@ -21,30 +24,49 @@ export default function PokemonComponent(props : Props) {
                    <Image src={pokemon.mainImage} thumbnail />
                </Col>
                <Col>
-                   Pokémon Properties
-               </Col>
+               <Stack gap = {2}>
+                   <ProgressBar>
+                   <ProgressBar variant="info" animated now={pokemon.speed} label={`${pokemon.speed}`} />
+                   <ProgressBar className="progress-bar-grey" animated now = {200 - pokemon.speed} label={`Speed`} />
+                   </ProgressBar>
+                   <ProgressBar>
+                   <ProgressBar variant="success" animated now={pokemon.healthPoints} label={`${pokemon.healthPoints}`} />
+                   <ProgressBar className="progress-bar-grey" animated now = {200 - pokemon.healthPoints} label={`Health`} />
+                   </ProgressBar>
+                   <ProgressBar>
+                   <ProgressBar variant="danger" animated now={pokemon.attack} label={`${pokemon.attack}`} />
+                   <ProgressBar className="progress-bar-grey" animated now = {200 - pokemon.attack} label={`Attack`} />
+                   </ProgressBar>
+                   <ProgressBar>
+                   <ProgressBar variant="warning" animated now={pokemon.defense} label={`${pokemon.defense}`} />
+                   <ProgressBar className="progress-bar-grey" animated now = {200 - pokemon.defense} label={`Defence`} />
+                   </ProgressBar>
+                   <Accordion defaultActiveKey="0" className="custom-accordion">
+                     <Accordion.Item eventKey="0">
+                        <Accordion.Header>Pokemon Type</Accordion.Header>
+                        <Accordion.Body>
+                        <Col md="auto">{pokemon.pokemonType[0]}</Col>
+                        <Col md="auto">{pokemon.pokemonType[1]}</Col>
+                        </Accordion.Body>
+                     </Accordion.Item>
+                  </Accordion>
+                  <Row>
+                  <Accordion defaultActiveKey="0" className="custom-accordion">
+                     <Accordion.Item eventKey="0">
+                        <Accordion.Header>Evolution Family</Accordion.Header>
+                        <Accordion.Body>
+                        <Col md="auto">{pokemon.evolutionFamily[0]} <Badge bg="danger">Devolution</Badge> </Col>
+                        <Col md="auto">{pokemon.evolutionFamily[1]} <Badge bg="secondary">Current</Badge> </Col>
+                        <Col md="auto">{pokemon.evolutionFamily[2]} <Badge bg="success">Evolution</Badge> </Col>
+                        </Accordion.Body>
+                     </Accordion.Item>
+                  </Accordion>
+                  </Row>
+               </Stack>    
+               </Col>       
            </Row>
-       </Container>
+       </Container>   
    );
 }
 
-import Accordion from 'react-bootstrap/Accordion';
-
-function AllCollapseExample() {
-  return (
-    <Accordion>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Strength
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Strength
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-  );
-}
+export default PokemonComponent;
